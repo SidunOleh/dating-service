b2d886c88e10874c9b9218ecf6a0fa4f9dc74b8f
Oleh Sidun <sidunoleh@gmail.com>
2024-07-23 01:42:58 +0300

    fix

--- resources/views/templates/rejected.blade.php
+++ resources/views/templates/rejected.blade.php
@@ -8,10 +8,12 @@ $comments = array_filter($comments, fn ($comment) => $comment);
             {{ 'Rejected' }}
         @endif
 
-        @foreach($comments as $comment)
+        @foreach($comments as $i => $comment)
             {{ $comment }}
+            @if ($i != array_count($comments)-1)
             <br>
             <br>
+            @endif
         @endforeach
     </p>
 </div>

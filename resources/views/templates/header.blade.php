af3cb2d2c42e501c63418b5dd68e34600fd5a6df
Oleh Sidun <sidunoleh@gmail.com>
2024-08-10 00:28:38 +0300

    fix

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/fancybox.css') }}"/>
-    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+    <!-- <link rel="stylesheet" href="{{ asset('assets/css/fancybox.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" /> -->
 </head>
 
 <body @class(['height' => Route::currentRouteName() == 'settings.page' ])>
+
+    @include('templates.css')
 
     <script>
         const DS = {}

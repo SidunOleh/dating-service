05f70d32ddb56282f0f9ac52663ef581f12ad2c3
Oleh Sidun <sidunoleh@gmail.com>
2024-08-10 00:18:02 +0300

    fix

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/fancybox.css') }}"/>
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
 </head>
 

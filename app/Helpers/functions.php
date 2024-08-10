a0d61b314674e47a5f690e77eef4fc5de6f85f6e
Oleh Sidun <sidunoleh@gmail.com>
2024-08-10 15:27:46 +0300

    fix

---
+++ app/Helpers/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function minify_css($css) {
+    $css = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css);
+    $css = preg_replace('/\s*([{}|:;,])\s+/', '$1', $css);
+    $css = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $css);
+   
+   return $css;
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -65,7 +65,7 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('embedstyles', function ($expression) {
             $params = explode(',', $expression);
 
-            return "<?php if ({$params[0]}): ?><style><?php echo file_get_contents({$params[1]}); ?></style><?php endif; ?>";
+            return "<?php if ({$params[0]}): ?><style><?php echo minify_css(file_get_contents({$params[1]})); ?></style><?php endif; ?>";
         });
 
         DB::listen(fn($sql) => $GLOBALS['sql'][] = $sql);

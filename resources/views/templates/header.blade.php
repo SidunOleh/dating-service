add6bee1db3aca212a7045767c17076646466019
Oleh Sidun <sidunoleh@gmail.com>
2024-08-10 23:25:23 +0300

    fix

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -2,15 +2,6 @@
 <html lang="en">
 
 <head>
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PXXCB3JB');</script>
-    <!-- End Google Tag Manager -->
-
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -22,12 +13,17 @@
     @embedstyles(true, 'assets/css/main.css')
 </head>
 
-<body @class(['height' => Route::currentRouteName() == 'settings.page',])>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GHJRHLJE88"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXXCB3JB"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+  gtag('config', 'G-GHJRHLJE88');
+</script>
+
+<body @class(['height' => Route::currentRouteName() == 'settings.page',])>
 
     <script>
         const DS = {}

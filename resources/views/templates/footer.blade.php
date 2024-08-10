a80418cec08fb9ea7f0ce3c73f63a0286a49ae08
Oleh Sidun <sidunoleh@gmail.com>
2024-08-10 03:40:29 +0300

    fix

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -17,7 +17,7 @@
 }
 </style>
 
-<script src="https://www.google.com/recaptcha/api.js?render={{ env('RECAPTCHA_SITE_KEY') }}" data-type="lazy"></script>
+<script data-src="https://www.google.com/recaptcha/api.js?render={{ env('RECAPTCHA_SITE_KEY') }}" data-type="lazy"></script>
 <script>
 function getReCaptchaV3(action) {
     return new Promise((resolve, reject) => grecaptcha.ready(() => grecaptcha

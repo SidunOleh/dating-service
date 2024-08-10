73915f7d764a63931b7fb0ac8adea0778a3438f0
Oleh Sidun <sidunoleh@gmail.com>
2024-08-10 04:01:20 +0300

    fix

--- app/Http/Controllers/Web/Pages/HomeController.php
+++ app/Http/Controllers/Web/Pages/HomeController.php
@@ -8,7 +8,6 @@ use App\Models\Option;
 use App\Models\Template;
 use App\Models\ZipCode;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {

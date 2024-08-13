d6ae60f8555deab21dc8296f042d873e900d7614
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2024-08-13 17:27:36 +0300

    quick commit

--- resources/views/pages/my-profile/create.blade.php
+++ resources/views/pages/my-profile/create.blade.php
@@ -861,6 +861,7 @@
                             </div>
 
                             <div class="input-wrapper">
+                                <img src="{{ asset('/assets/img/select.svg') }}" alt=""  class='select-img'/> 
                                 <label for="gender">
                                     Your gender:
                                 </label>

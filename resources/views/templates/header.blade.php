c37cd709d3b6a3e839bf176ffe121d176003b2ec
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2024-08-14 13:31:09 +0300

    quick commit

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -95,7 +95,9 @@
                             placeholder="Search" 
                             value="{{ request()->query('s') }}"/>
                         <button type="submit" class="search-button">
-                            <img src="{{ asset('assets/img/search.svg') }}" alt="Search" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M16.8931 16.92L19.9731 20M19.0841 11.581C19.0841 15.768 15.7011 19.162 11.5291 19.162C7.35614 19.162 3.97314 15.768 3.97314 11.582C3.97314 7.393 7.35614 4 11.5281 4C15.7011 4 19.0841 7.394 19.0841 11.581Z" stroke="#BEBFC0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>  
                         </button>
                     </form>
                 </div>

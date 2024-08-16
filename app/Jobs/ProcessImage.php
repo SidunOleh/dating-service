097a87d55725019cf597d174106f8c9b50d8fe5a
Oleh Sidun <sidunoleh@gmail.com>
2024-08-16 22:18:53 +0300

    fix

--- app/Jobs/ProcessImage.php
+++ app/Jobs/ProcessImage.php
@@ -42,16 +42,25 @@ class ProcessImage implements ShouldQueue
         $manager->read($this->image->getPath())->toWebp($this->quality)->save($this->image->getPath());
 
         if ($this->watermark) {
-            SpatieImage::load($this->image->getPath())->watermark(
-                storage_path('watermark.png'),
-                AlignPosition::Center,
-                width: 100,
-                widthUnit: Unit::Percent,
-                height: 100,
-                heightUnit: Unit::Percent,
-                fit: Fit::Stretch,
-                alpha: 10,
-            )->save();
+            // SpatieImage::load($this->image->getPath())->watermark(
+            //     storage_path('watermark.png'),
+            //     AlignPosition::Center,
+            //     width: 100,
+            //     widthUnit: Unit::Percent,
+            //     height: 100,
+            //     heightUnit: Unit::Percent,
+            //     fit: Fit::Stretch,
+            //     alpha: 10,
+            // )->save();
+
+            $image = $manager->read($this->image->getPath());
+
+            $watermark = $manager->read(storage_path('watermark.png'))->cover(
+                $image->width(),
+                $image->height()
+            );
+
+            $image->place($watermark, 'center')->save($this->image->getPath());
         }
 
         $this->image->update(['processed' => true,]);

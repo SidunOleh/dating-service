e37fa7d280cc4009c026bc9f3e780e3c2e8ae0c6
Oleh Sidun <sidunoleh@gmail.com>
2024-08-31 01:40:20 +0300

    fix

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
@@ -26,6 +27,13 @@ class Template extends Model
     private Collection $data;
 
     private int $total;
+
+    protected function firstName(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => json_decode($value, true),
+        );
+    }
 
     public function count(string $block): int
     {

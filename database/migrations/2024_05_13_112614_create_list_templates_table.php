cd1167851d9ddab898d8eca3d90e4299f1c5c7f8
Oleh Sidun <sidunoleh@gmail.com>
2024-08-31 00:58:14 +0300

    fix

--- database/migrations/2024_05_13_112614_create_list_templates_table.php
+++ database/migrations/2024_05_13_112614_create_list_templates_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('templates', function (Blueprint $table) {
             $table->id();
-            $table->json('template');
+            $table->text('template');
             $table->timestamps();
         });
     }

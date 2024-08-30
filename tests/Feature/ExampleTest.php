f235f53a79ee828f1c1499dcc4085afb079244bf
Oleh Sidun <sidunoleh@gmail.com>
2024-08-31 02:17:06 +0300

    fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Models\Template;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -20,6 +21,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        Template::create(['template' => '[]']);
+
         $response = $this->get('/');
 
         $response->assertStatus(200);

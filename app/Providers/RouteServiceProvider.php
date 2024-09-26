7c48f67e576deb399cf76739a01b8515637cfd89
Oleh Sidun <sidunoleh@gmail.com>
2024-09-26 18:59:37 +0300

    fi

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('cLAhDKeUKrDErLAyUS21')
+                ->prefix('api/cLAhDKeUKrDErLAyUS21')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')

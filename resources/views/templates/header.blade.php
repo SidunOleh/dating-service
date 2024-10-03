2e66121721801eef18ccfcf7f370d72fa27c8a2a
Oleh Sidun <sidunoleh@gmail.com>
2024-10-03 23:29:15 +0300

    fic

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{ config('app.name') }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title>{{ ($title ?? '') ? $title . ' | ' . config('app.name') : config('app.name') }}</title>
 

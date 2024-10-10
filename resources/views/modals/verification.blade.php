bfdcd11bf474a90ea3460d797c6acde7068504d1
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2024-10-10 14:54:27 +0300

    quick commit

--- resources/views/modals/verification.blade.php
+++ resources/views/modals/verification.blade.php
@@ -1,5 +1,8 @@
 <div class="verification-wrapper">
     <div class="verification-container">
+        <div class="close">
+            <img src="{{ asset('/assets/img/close.svg') }}" alt="">
+        </div>
         <p class="title">
             I sent the code, please check your Email:
         </p>

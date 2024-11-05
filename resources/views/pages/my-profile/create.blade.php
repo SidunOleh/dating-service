4f1499bf0d7700eebd3bb6900ce07633ce930a83
Oleh Sidun <sidunoleh@gmail.com>
2024-11-05 14:51:08 +0200

    fix

--- resources/views/pages/my-profile/create.blade.php
+++ resources/views/pages/my-profile/create.blade.php
@@ -948,12 +948,12 @@
                                             src="/assets/img/img-loading.webp">
 
                                         <button 
-                                            v-if="data.photos.length" 
+                                            v-if="data.photos.length > 1" 
                                             class="move-button move-up" 
                                             type="button" 
                                             @click="moveUp(i)">↑</button>
                                         <button 
-                                            v-if="data.photos.length" 
+                                            v-if="data.photos.length > 1" 
                                             class="move-button move-down" 
                                             type="button" 
                                             @click="moveDown(i)">↓</button>

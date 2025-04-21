fcc9e2632996299ed2d38257415e1adeebe4f3c9
Oleh Sidun <sidunoleh@gmail.com>
2025-04-21 17:45:13 +0300

    fix

--- app/Http/Controllers/Web/Balances/TransferController.php
+++ app/Http/Controllers/Web/Balances/TransferController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\Web\Balances;
 
 use App\Exceptions\NotEnoughMoneyException;
 use App\Http\Controllers\Controller;
+use App\Models\Creator;
 use App\Services\Balances\BalancesService;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class TransferController extends Controller
@@ -21,9 +22,13 @@ class TransferController extends Controller
     public function __invoke()
     {
         try {
-            $creator = Auth::guard('web')->user();
+            DB::beginTransaction();
+
+            $creator = Creator::lockForUpdate()->find(Auth::guard('web')->id());
 
             $this->balancesService->transferBalanceBalance2($creator, 1);
+
+            DB::commit();
 
             return response([
                 'balance' => $creator->balance, 
